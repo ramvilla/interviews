6690fe76cb0fc87b8a98fe1285d80eb9ec802c5f
ramvilla <rohit.batra.er@gmail.com>
2016-10-12 14:14:14 -0400

    Update Exercise1.java

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -5,13 +5,13 @@ package com.elsevier.education;
 TODO: Make this class immutable.
 
 */
-public class Exercise1 {
+public Final class Exercise1 {
 
 	public static class Person {
-		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		//seeting the data member as final and removing the setter method
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
 		public Person() {
 		}
@@ -19,22 +19,22 @@ public class Exercise1 {
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+		/*public void setPhoneNumbers(Set<String> newPhoneNumbers) {
 			phoneNumbers = newPhoneNumbers;
-		}
+		}*/
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
+		/*public void setFirstName(String newName) {
 			firstName = newName;
-		}
+		}*/
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
+		/*public void setLastName(String newName) {
 			lastName = newName;
-		}
+		}*/
 	}
 }
